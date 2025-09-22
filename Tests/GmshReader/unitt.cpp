e43ecd3f7866deaab27d91217ecb261f25e82e9c
Lucas Gasparino <lucas.gasparino@bsc.es>
2025-09-22 16:34:04 +0200

    Added dummy CMakeLists to allow directory in Git. Added initial testingfor GmshReader.

--- Tests/GmshReader/unitt.cpp
+++ Tests/GmshReader/unitt.cpp
@@ -5,4 +5,35 @@ int main() {
     std::string fPath = "/home/lucas/Documents/GLASs/Tests/GmshReader/";
     GmshReader<uint32_t, float> test(fName, fPath);
     test.readMesh();
+    uint32_t nElems = test.getNumElements();
+    if (nElems != 16) {
+        std::cerr << "Error: number of elements read is incorrect (" << nElems << " != 16)" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    uint32_t nPoints = test.getNumPoints();
+    if (nPoints != 25) {
+        std::cerr << "Error: number of points read is incorrect (" << nPoints << " != 25)" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    uint32_t nBoundElems = test.getNumBoundElements();
+    if (nBoundElems != 16) {
+        std::cerr << "Error: number of boundary elements read is incorrect (" << nBoundElems << " != 16)" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    uint32_t** testPtrInt = test.getElemTable();
+    if (testPtrInt == nullptr) {
+        std::cerr << "Error: element table pointer is null" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    testPtrInt = test.getBoundElemTable();
+    if (testPtrInt == nullptr)
+    {
+        std::cerr << "Error: element table pointer is null" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    float** testPtrFloat = test.getCoordsTable();
+    if (testPtrFloat == nullptr) {
+        std::cerr << "Error: coordinates table pointer is null" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 }
