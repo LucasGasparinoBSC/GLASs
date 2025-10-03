960ecbafc6afc907acd6790b03c22e4261650f7e
Lucas Gasparino <lucas.gasparino@bsc.es>
2025-10-03 10:48:38 +0200

    .... fixed missing D... :(

--- Tests/ConjugateGradient/unitt_32.cpp
+++ Tests/ConjugateGradient/unitt_32.cpp
@@ -4,5 +4,13 @@
 #include "ConjugateGradient.hpp"
 
 int main() {
+
+    // Problem definitions
+    uint32_t arrSize = 100;
+    uint32_t mIters = 10;
+    double tol = 1e-10;
+
+    // Instantiate the solver
+    ConjugateGradient<uint32_t, float> solver(arrSize, mIters, tol);
     return 0;
 }
