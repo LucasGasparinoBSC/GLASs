24ef4c04879f7b88d0a76940dea6d499afe9347a
Adria Quintanas Corominas <adria.quintanas@bsc.es>
2025-10-12 17:01:42 +0200

    Add Elastodynamics 1D application, IMEX solver, #16

--- src/Apps/Elastodynamics1D/mod_Elastodynamics1D.F90
+++ src/Apps/Elastodynamics1D/mod_Elastodynamics1D.F90
@@ -6,6 +6,8 @@ module mod_ElastoDynamics1D
 
     implicit none
 
+    integer(ip) :: GLASS_ED1D_OUT_UNIT = 19900502
+
     type ElastoDynamics1D_t
         ! Physical properties
         real(rp)          :: E     ! Young modulus
@@ -15,6 +17,9 @@ module mod_ElastoDynamics1D
         ! Physical parameters (derived)
         real(rp)          :: Le    ! Element lentgh
         real(rp)          :: c     ! Elastic wave velocity
+        ! Harmonic excitation
+        real(rp)          :: H     ! Amplitude 
+        real(rp)          :: w     ! Period
         ! Time integration parameters
         real(rp)          :: time
         real(rp)          :: dtime
@@ -36,6 +41,9 @@ module mod_ElastoDynamics1D
         ! CG parameters
         integer(ip)       :: niter
         real(dp)          :: toler
+        ! Output
+        integer(ip)       :: out_freq
+        integer(4)        :: out_unit
     contains
         procedure, pass :: free => ElastoDynamics1D_free
         procedure, pass :: initialize => ElastoDynamics1D_initialize
@@ -46,6 +54,7 @@ module mod_ElastoDynamics1D
         procedure, pass :: applyFMOp => ElastoDynamics1D_applyFreeMatrixOperator
         procedure, pass :: computeCoeffs => ElastoDynamics1D_computeCoeffs
         procedure, pass :: computeExternalForce => ElastoDynamics1D_computeExternalForce
+        procedure, pass :: outputNodeSets => ElastoDynamics1D_outputNodeSets
     end type ElastoDynamics1D_t
 
     private
@@ -74,6 +83,8 @@ module mod_ElastoDynamics1D
         this%dtime = 0.0_rp
         this%nnode = 0_ip
         this%nelem = 0_ip
+        this%H = 0.0_rp
+        this%w = 0.0_rp
         nullify(this%stifm)
         nullify(this%massm)
         this%npoin = 0_ip
@@ -97,31 +108,35 @@ module mod_ElastoDynamics1D
         !$acc enter data copyin(this)
 
         ! Input parameters
-        this%E = 270.0e9_rp
+        this%E = 210.0e9_rp
         this%A = 1.0e-4_rp
         this%rho = 7800.0_rp
-        this%L = 0.1_rp 
-        this%dtime = 1.0e-4_rp
-        this%nsteps = 100_ip
+        this%L = 1.0_rp 
         this%beta = 0.25_rp
         this%gamma = 0.5_rp
-        this%alpha = 1.0_rp
+        this%H = 1000.0_rp
+        this%w = 5.0e-4_rp
+
+        this%dtime = 1.0e-6_rp
+        this%nsteps = 10000_ip
         this%time = 0.0_rp
 
         ! Derived types
         denom = this%beta * this%dtime * this%dtime
         if( abs(denom) > tiny(0.0_rp) )then
             this%alpha = 1.0_rp / denom
+        else
+            this%alpha = 1.0_rp
         endif
 
         ! Nodes per element
         this%nnode = 2_ip
 
         ! Elemental arrays
-        this%nelem = 50_ip 
+        this%nelem = 100_ip 
         allocate(this%stifm(this%nelem), source=0.0_rp)
         allocate(this%massm(this%nelem), source=0.0_rp)
-        !$acc enter data copyin(this%ke, this%me)
+        !$acc enter data copyin(this%stifm, this%massm)
 
         ! Nodal arrays
         this%npoin = this%nelem + 1_ip
@@ -134,10 +149,15 @@ module mod_ElastoDynamics1D
         ! Derived parameters
         this%le = this%L/real(this%nelem,kind=rp)
         this%c = sqrt(this%E/this%rho)
+        this%dtime = 0.50_rp * this%le / this%c
 
         ! SOlver
-        this%niter = 10000_ip
+        this%niter = 100_ip
         this%toler = 1.0e-12_dp
+
+        ! Output
+        this%out_freq = 1_ip
+        this%out_unit = GLASS_ED1D_OUT_UNIT
 
     end subroutine ElastoDynamics1D_initialize
 
@@ -162,42 +182,33 @@ module mod_ElastoDynamics1D
         
         class(ElastoDynamics1D_t), intent(inout) :: this
 
-        real(rp), contiguous, pointer :: u1_p(:), u0_p(:)
-        real(rp), contiguous, pointer :: v1_p(:), v0_p(:)
-        real(rp), contiguous, pointer :: a1_p(:), a0_p(:)
-        real(rp), contiguous, pointer :: b_p(:)
+        real(rp), contiguous, pointer :: u1_p(:)
+        real(rp), contiguous, pointer :: v1_p(:)
+        real(rp), contiguous, pointer :: a1_p(:)
+        real(rp), contiguous, pointer :: fe_p(:)
+        real(rp), contiguous, pointer :: r_p(:)
         real(rp), contiguous, pointer :: x_p(:)
+        real(rp), contiguous, pointer :: y_p(:)
 
         integer(ip)  :: n, p
-        real(rp) :: cd1, cd2, cv1, cv2, ca1
 
         type(c_ptr)    :: cgSolver
         type(c_ptr)    :: opData
         type(c_funptr) :: opFunction
 
         ! Create auxiliar arrays
-        allocate(u0_p(this%npoin), source=0.0_rp)
-        allocate(v0_p(this%npoin), source=0.0_rp)
-        allocate(a0_p(this%npoin), source=0.0_rp)
-        allocate(b_p(this%npoin), source=0.0_rp)
+        allocate(r_p(this%npoin), source=0.0_rp)
         allocate(x_p(this%npoin), source=0.0_rp)
-        !$acc enter data copyin(u0_p, v0_p, a1_p, b_p, x_p)
+        !$acc enter data copyin(r_p, x_p)
 
         ! Recover some pointers
         u1_p => this%displ
         v1_p => this%veloc
         a1_p => this%accel
-
-        ! Constant coefficients
-        cd1 = this%dtime
-        cd2 = this%dtime * this%dtime * (0.5 - this%beta)
-        cv1 = this%dtime * (1.0_rp - this%gamma)
-        cv2 = this%dtime * this%gamma
-        ca1 = this%alpha
+        fe_p => this%fexte
 
         ! Create solver instance and setup
-        !cgSolver = cg_create_u32_f(this%npoin, this%niter, this%toler)
-        !call cg_setup_u32_f(cgSolver, u0_p, b_p)
+        cgSolver = cg_create_u32_f(this%npoin, this%niter, this%toler)
 
         ! Get function pointer for matvec
         opFunction = c_funloc(ElastoDynamics1D_operator_c)
@@ -220,68 +231,69 @@ module mod_ElastoDynamics1D
             ! Update time
             this%time = this%time + this%dtime
             !
-            ! Compute predictors
+            ! Update temporal variables
+            fe_p(this%npoin) = fe_p(this%npoin) + this%H * 6.0_rp / 0.005_rp * cos(6.0_rp * this%time/0.005_rp) * this%dtime
+            !
+            ! Update displacement
+            !$acc parallel loop present(u1_p, v1_p, a1_p)
             do p = 1, this%npoin
-                u1_p(p) = u0_p(p) + cd1 * v1_p(p) + cd2 * a0_p(p)
-                v1_p(p) = v0_p(p) + cv1 * a0_p(p)
+                u1_p(p) = u1_p(p) + this%dtime * (v1_p(p) + 0.5_rp * this%dtime * a1_p(p))
             end do
+            u1_p(1) = 0.0_rp
             !
             ! Assemble RHS
-            !   b = fext + alpha * M * u
-            call this%cleanVect(b_p)
-            call this%assembleRHS(u1_p, b_p)
+            !   r = fext - K * u
+            call this%cleanVect(r_p)
+            call this%assembleRHS(u1_p, r_p)
+            r_p(1) = 0.0_rp
             !
-            ! Solve (K + alpha M) u_{n+1} = rhs  on free set
-            cgSolver = cg_create_u32_f(this%npoin, this%niter, this%toler)
-            call cg_setup_u32_f(cgSolver, u1_p, b_p)
+            ! Solve M u_{n+1} = rhs  on free set
+            call cg_setup_u32_f(cgSolver, a1_p, r_p)
             call cg_solve_u32_f(cgSolver, opFunction, opData)
             call cg_get_solution_u32_f(cgSolver, x_p)
-            call cg_destroy_u32_f(cgSolver)
+            a1_p(1) = 0.0_rp
             !
             ! Correct predictions
+            !$acc parallel loop present(x_p, v1_p, a1_p)
             do p = 1, this%npoin
-                a1_p(p) = ca1 * (x_p(p) - u1_p(p))
-                v1_p(p) = v0_p(p) + cv2 * a0_p(p)
-                u1_p(p) = x_p(p)
+                v1_p(p) = v1_p(p) + 0.5_rp * this%dtime * (x_p(p) + a1_p(p))
+                a1_p(p) = x_p(p)
             end do
-
-            write(99,*) u1_p(this%npoin), v1_p(this%npoin), a1_p(this%npoin)
-
-            ! Update time arrays
-            u0_p = u1_p
-            v0_p = v1_p
-            a0_p = a1_p
+            v1_p(1) = 0.0_rp
+            !
+            ! Output monitoring node
+            if( mod(n,this%out_freq) == 0_ip )then
+                call this%outputNodeSets()
+            endif
 
         end do
 
         ! Free solver
-        !call cg_destroy_u32_f(cgSolver)
+        call cg_destroy_u32_f(cgSolver)
 
         ! Free auxiliar arrays
-        deallocate(u0_p)
-        deallocate(v0_p)
-        deallocate(a0_p)
-        deallocate(b_p)
+        deallocate(x_p)
+        deallocate(r_p)
         
     end subroutine ElastoDynamics1D_run
 
     
-    subroutine ElastoDynamics1D_assembleRHS(this, u_p, b_p)
+    subroutine ElastoDynamics1D_assembleRHS(this, u_p, r_p)
         
         class(ElastoDynamics1D_t), intent(inout) :: this
         real(rp),                  intent(in)    :: u_p(this%npoin)
-        real(rp),                  intent(inout) :: b_p(this%npoin)
+        real(rp),                  intent(inout) :: r_p(this%npoin)
 
         integer(ip) :: e, p, pi, pj
         real(rp)    :: ui, uj 
-        real(rp)    :: bi, bj
+        real(rp)    :: ri, rj
 
-        real(rp), contiguous, pointer :: m_e(:)
+        real(rp), contiguous, pointer :: k_e(:)
         real(rp), contiguous, pointer :: f_p(:)
 
         ! Add intertial force contribution
-        m_e => this%massm
-        !$-acc parallel loop present(u,y,) gang vector
+        k_e => this%stifm
+        !$acc parallel loop present(k_e, u_p, r_p)
         do e = 1, this%nelem
             ! Global indices
             pi = e
@@ -290,64 +302,59 @@ module mod_ElastoDynamics1D
             ui = u_p(pi)
             uj = u_p(pj)
             ! Compute elemental contribution
-            !    Me * ue = m * [[2,1],[1,2]] * [ui, uj]^T
-            bi = this%alpha * m_e(e) * (2.0_rp * ui + uj)
-            bj = this%alpha * m_e(e) * (2.0_rp * uj + ui)
+            !    re += Ke * ue = ke * [[1,-1],[-1,1]] * [ui, uj]^T
+            ri = k_e(e) * (ui - uj)
+            rj = k_e(e) * (uj - ui)
             ! Assemble to global
             !$acc atomic update
-            b_p(pi) = b_p(pi) + bi
+            r_p(pi) = r_p(pi) - ri
             !$acc atomic update
-            b_p(pj) = b_p(pj) + bj
+            r_p(pj) = r_p(pj) - rj
         end do
 
         ! Add body/external force
         f_p => this%fexte
-        !$acc parallel loop present(b_p, f_p)
+        !$acc parallel loop present(r_p, f_p)
         do p = 1, this%npoin
-            b_p(p) = b_p(p) + f_p(p)
+            r_p(p) = r_p(p) + f_p(p)
         end do
 
     end subroutine ElastoDynamics1D_assembleRHS
 
     
-    subroutine ElastoDynamics1D_applyFreeMatrixOperator(this, u_p, y_p)
+    subroutine ElastoDynamics1D_applyFreeMatrixOperator(this, x_p, y_p)
         
         class(ElastoDynamics1D_t), intent(inout) :: this
-        real(rp),                  intent(in)    :: u_p(this%npoin)
+        real(rp),                  intent(in)    :: x_p(this%npoin)
         real(rp),                  intent(inout) :: y_p(this%npoin)
 
         integer(ip) :: e, pi, pj
-        real(rp)    :: ui, uj 
-        real(rp)    :: bi, bj
+        real(rp)    :: xi, xj 
+        real(rp)    :: yi, yj
 
         real(rp), contiguous, pointer :: m_e(:)
-        real(rp), contiguous, pointer :: k_e(:)
 
         ! Recover some pointers
         m_e => this%massm
-        k_e => this%stifm
 
-        !$-acc parallel loop present(u,y,) gang vector
+        !$acc parallel loop present(m_e, x_p, y_p)
         do e = 1, this%nelem
             ! Global indices
             pi = e
             pj = e + 1_ip
-            ! Gather displaceent
-            ui = u_p(pi)
-            uj = u_p(pj)
-            ! Compute stiffness contribution
-            !    Ke * ue = k * [[1,-1],[-1,1]] * [ui, uj]^T
-            bi = k_e(e) * (ui - uj)
-            bj = k_e(e) * (uj - ui)
+            ! Gather acceleration
+            xi = x_p(pi)
+            xj = x_p(pj)
             ! Compute mass contribution
-            !    Me * ue = m * [[2,1],[1,2]] * [ui, uj]^T
-            bi = bi + this%alpha * m_e(e) * (2.0_rp * ui + uj)
-            bj = bj + this%alpha * m_e(e) * (2.0_rp * uj + ui)
+            !    Me * ae = m * [[2,1],[1,2]] * [ai, aj]^T
+            !    x is the nodal acceleration
+            yi = m_e(e) * (2.0_rp * xi + xj)
+            yj = m_e(e) * (2.0_rp * xj + xi)
             ! Assemble to global
             !$acc atomic update
-            y_p(pi) = y_p(pi) + bi
+            y_p(pi) = y_p(pi) + yi
             !$acc atomic update
-            y_p(pj) = y_p(pj) + bj
+            y_p(pj) = y_p(pj) + yj
         end do
 
     end subroutine ElastoDynamics1D_applyFreeMatrixOperator
@@ -377,13 +384,17 @@ module mod_ElastoDynamics1D
         real(rp), contiguous, pointer :: m_e(:)
         real(rp), contiguous, pointer :: k_e(:)
 
-        ! Recover some pointers
-        m_e => this%massm
+        ! Elemental matrix
         k_e => this%stifm
-
-        !$-acc parallel loop present(u,y,) gang vector
+        !$acc parallel loop present(k_e)
         do e = 1, this%nelem
             k_e(e) = this%E * this%A / this%Le
+        end do
+
+        ! Elemental stiffness
+        m_e => this%massm
+        !$acc parallel loop present(m_e)
+        do e = 1, this%nelem
             m_e(e) = this%rho * this%A * this%Le / 6.0_rp
         end do
 
@@ -403,17 +414,29 @@ module mod_ElastoDynamics1D
         m_e => this%massm
         f_p => this%fexte
 
-        !$-acc parallel loop present(u,y,) gang vector
+        !$acc parallel loop present(m_e, f_p)
         do e = 1, this%nelem
             ! Global indices
             pi = e
             pj = e + 1_ip
             ! Gracity force
+            !$acc atomic update
             f_p(pi) = f_p(pi) + 3.0_rp * 9.81_rp * m_e(e)
+            !$acc atomic update
             f_p(pj) = f_p(pj) + 3.0_rp * 9.81_rp * m_e(e)
         end do
 
     end subroutine ElastoDynamics1D_computeExternalForce
+
+
+    subroutine ElastoDynamics1D_outputNodeSets(this)
+        
+        class(ElastoDynamics1D_t), intent(inout) :: this
+
+        !$acc update host(this%time, this%displ(this%npoin), this%veloc(this%npoin), this%accel(this%npoin))
+        write(this%out_unit, '(4(E22.15,2x))') this%time, this%displ(this%npoin), this%veloc(this%npoin), this%accel(this%npoin)
+
+    end subroutine ElastoDynamics1D_outputNodeSets
 
 
     subroutine ElastoDynamics1D_operator_c(x_in, x_out, opData) bind(C)
@@ -429,5 +452,6 @@ module mod_ElastoDynamics1D
         call ED1D%applyFMOp(x_in, x_out)
  
     end subroutine ElastoDynamics1D_operator_c
+    
 
 end module mod_ElastoDynamics1D
