22d4344fc8fb4743fd2728ad9aea947164ba4d63
Lucas Gasparino <lucas.gasparino@bsc.es>
2025-10-13 13:41:12 +0200

    fpcg added with no testing.

--- src/Libs/LinAlgebra/IterSolvers/sources/ConjugateGradient.cpp
+++ src/Libs/LinAlgebra/IterSolvers/sources/ConjugateGradient.cpp
@@ -278,6 +278,7 @@ void ConjugateGradient<ITYPE, RTYPE>::fpcgSolver(const MatVecOp &matvec, const P
     CUDA_CHECK(cudaMemset(this->d_r0, 0, this->arrSize * sizeof(RTYPE)));
     CUDA_CHECK(cudaMemset(this->d_p0, 0, this->arrSize * sizeof(RTYPE)));
     CUDA_CHECK(cudaMemset(this->d_rk, 0, this->arrSize * sizeof(RTYPE)));
+    CUDA_CHECK(cudaMemset(this->d_zk, 0, this->arrSize * sizeof(RTYPE)));
     CUDA_CHECK(cudaMemset(this->d_Ax, 0, this->arrSize * sizeof(RTYPE)));
     CUDA_CHECK(cudaMemset(this->d_res0, 0, 1 * sizeof(RTYPE)));
     CUDA_CHECK(cudaMemset(this->d_resk, 0, 1 * sizeof(RTYPE)));
@@ -301,30 +302,37 @@ void ConjugateGradient<ITYPE, RTYPE>::fpcgSolver(const MatVecOp &matvec, const P
     launchKernel(axpy<ITYPE, RTYPE>, grid, block, negOne, this->d_Ax, this->d_r0, this->arrSize); // r0 = r0 - Ax
     POP_RANGE
 
-    // 2. compute initial residual norm |r0| and initialize rk, rk.rk, p0
+    // 2. Precondition M z0 = r0
+    PUSH_RANGE("cgSolver: precond", 3)
+    precond(this->d_r0, this->d_zk); // zk = M^-1 * r0
+    POP_RANGE
+
+    // 3. compute initial residual norm |r0| and initialize rk, rk.rk, p0
     PUSH_RANGE("cgSolver: residual0", 3)
     launchKernel(copy_array<ITYPE, RTYPE>, grid, block, this->d_r0, this->d_rk, this->arrSize); // rk = r0
     launchKernel(copy_array<ITYPE, RTYPE>, grid, block, this->d_r0, this->d_p0, this->arrSize); // p0 = r0
     CUDA_CHECK(cudaMemset(this->d_tmpDot, 0, 1 * sizeof(double)));
     launchKernel(dot_product<ITYPE, RTYPE>, grid, block, this->d_r0, this->d_r0, this->d_tmpDot, this->arrSize); // tmpDot = r0 . r0
     CUDA_CHECK(cudaMemcpy(this->tmpDot, this->d_tmpDot, 1 * sizeof(double), cudaMemcpyDeviceToHost));
-    // TODO: finish this part
-    this->resk[0] = static_cast<RTYPE>(this->tmpDot[0]);            // resk = rk.rk at k = 0
     this->res0[0] = static_cast<RTYPE>(std::sqrt(this->tmpDot[0])); // res0 = |r0|
+    CUDA_CHECK(cudaMemset(this->d_tmpDot, 0, 1 * sizeof(double)));
+    launchKernel(dot_product<ITYPE, RTYPE>, grid, block, this->d_rk, this->d_zk, this->d_tmpDot, this->arrSize); // tmpDot = rk . zk
+    CUDA_CHECK(cudaMemcpy(this->tmpDot, this->d_tmpDot, 1 * sizeof(double), cudaMemcpyDeviceToHost));
+    this->resk[0] = static_cast<RTYPE>(this->tmpDot[0]); // resk = rk.zk at k = 0
     POP_RANGE
 
     POP_RANGE // 2: initial step
 
-        // Iterate
-        this->iter = 0;
+    // Iterate
+    this->iter = 0;
     PUSH_RANGE("cgSolver: iterations", 2)
     while (this->iter < this->maxIters)
     {
         PUSH_RANGE("cgSolver: iteration", 3)
         this->iter++; // increment iteration counter then compute
 
-        // 3. Compute alpha
-        //   -- rk.rk should already be computed, so only need pk.Apk
+        // 4. Compute alpha
+        //   -- rk.zk should already be computed, so only need pk.Apk
         PUSH_RANGE("cgSolver: alpha", 4)
         matvec(this->d_p0, this->d_Ax); // Ax = A*p0
         CUDA_CHECK(cudaMemset(this->d_tmpDot, 0, 1 * sizeof(double)));
@@ -333,39 +341,64 @@ void ConjugateGradient<ITYPE, RTYPE>::fpcgSolver(const MatVecOp &matvec, const P
         this->alpha[0] = this->resk[0] / static_cast<RTYPE>(this->tmpDot[0]);
         POP_RANGE // 4: alpha
 
-            // 4. Update solution xk = xk-1 + alpha*pk-1 and residual rk = rk-1 - alpha*Apk-1
-            PUSH_RANGE("cgSolver: update step", 4)
-                launchKernel(axpy<ITYPE, RTYPE>, grid, block, this->alpha[0], this->d_p0, this->d_x_sol, this->arrSize); // x_sol = x_sol + alpha*p0
+        // 5. Update solution xk = xk-1 + alpha*pk-1 and residual rk = rk-1 - alpha*Apk-1
+        PUSH_RANGE("cgSolver: update step", 4)
+        launchKernel(axpy<ITYPE, RTYPE>, grid, block, this->alpha[0], this->d_p0, this->d_x_sol, this->arrSize); // x_sol = x_sol + alpha*p0
         RTYPE negAlpha = -this->alpha[0];
         launchKernel(axpy<ITYPE, RTYPE>, grid, block, negAlpha, this->d_Ax, this->d_rk, this->arrSize); // rk = rk - alpha*Ax
         POP_RANGE                                                                                       // 4: update step
 
-            // 5. Compute new rk.rk
-            PUSH_RANGE("cgSolver: residualk", 4)
-                CUDA_CHECK(cudaMemset(this->d_tmpDot, 0, 1 * sizeof(double)));
+        // 6. Compute new rk.rk
+        PUSH_RANGE("cgSolver: residualk", 4)
+        CUDA_CHECK(cudaMemset(this->d_tmpDot, 0, 1 * sizeof(double)));
         launchKernel(dot_product<ITYPE, RTYPE>, grid, block, this->d_rk, this->d_rk, this->d_tmpDot, this->arrSize); // tmpDot = rk . rk
         CUDA_CHECK(cudaMemcpy(this->tmpDot, this->d_tmpDot, 1 * sizeof(double), cudaMemcpyDeviceToHost));
         this->aux[0] = static_cast<RTYPE>(this->tmpDot[0]); // aux = rk.rk
         POP_RANGE                                           // 4: residualk
 
-            // Check convergence
-            if (std::sqrt(this->tmpDot[0]) < this->tol * static_cast<double>(this->res0[0]))
+        // Check convergence
+        if (std::sqrt(this->tmpDot[0]) < this->tol * static_cast<double>(this->res0[0]))
         {
             // Converged
             printf("--| cgSolver: converged at iteration %u with residual %e\n", this->iter, static_cast<double>(std::sqrt(this->tmpDot[0])));
             POP_RANGE // 3: iteration
-                break;
+            break;
         }
 
-        // 6. Compute beta and update search direction pk = rk + beta*pk-1
-        PUSH_RANGE("cgSolver: beta", 4)
-        this->beta[0] = this->aux[0] / this->resk[0];                                                        // beta = (rk.rk)new / (rk.rk)old
-        launchKernel(copy_array<ITYPE, RTYPE>, grid, block, this->d_rk, this->d_p0, this->arrSize);          // p0 = rk
-        launchKernel(axpy<ITYPE, RTYPE>, grid, block, this->beta[0], this->d_p0, this->d_p0, this->arrSize); // p0 = rk + beta*p0
-        this->resk[0] = this->aux[0];                                                                        // resk = (rk.rk)new
-        POP_RANGE                                                                                            // 4: beta
+        // 7. Compute aux = rk+1 . zk
+        PUSH_RANGE("cgSolver: rk+1.zk ", 4)
+        CUDA_CHECK(cudaMemset(this->d_tmpDot, 0, 1 * sizeof(double)));
+        launchKernel(dot_product<ITYPE, RTYPE>, grid, block, this->d_rk, this->d_zk, this->d_tmpDot, this->arrSize); // tmpDot = rk . zk
+        CUDA_CHECK(cudaMemcpy(this->tmpDot, this->d_tmpDot, 1 * sizeof(double), cudaMemcpyDeviceToHost));
+        this->aux[0] = static_cast<RTYPE>(this->tmpDot[0]); // aux = rk+1 . zk
+        POP_RANGE // 4: rk+1.zk
 
-            POP_RANGE // 3: iteration
+        // 8. Precondition M * zk+1 = rk+1
+        PUSH_RANGE("cgSolver: precond", 4)
+        precond(this->d_rk, this->d_zk); // zk = M^-1 * rk
+        POP_RANGE // 4: precond
+
+        // 9. beta = (rk+1.zk+1)
+        PUSH_RANGE("cgSolver: beta prep", 4)
+        CUDA_CHECK(cudaMemset(this->d_tmpDot, 0, 1 * sizeof(double)));
+        launchKernel(dot_product<ITYPE, RTYPE>, grid, block, this->d_rk, this->d_zk, this->d_tmpDot, this->arrSize); // tmpDot = rk . zk
+        CUDA_CHECK(cudaMemcpy(this->tmpDot, this->d_tmpDot, 1 * sizeof(double), cudaMemcpyDeviceToHost));
+        this->beta[0] = static_cast<RTYPE>(this->tmpDot[0]); // beta = rk+1.zk+1
+        POP_RANGE // 4: beta prep
+
+        // 10. Compute beta = rk+1 . (zk+1 - zk) / rk . zk
+        RTYPE tmp = this->beta[0];
+        this->beta[0] = this->beta[0] - this->aux[0] / this->resk[0]; // beta = (rk+1.zk+1 - rk.zk) / rk.zk
+        this->resk[0] = tmp;
+
+        // 11. Update search direction pk = rk + beta*pk-1
+        PUSH_RANGE("cgSolver: Update pk", 4)
+        const RTYPE one = static_cast<RTYPE>(1);
+        launchKernel(scale<ITYPE, RTYPE>, grid, block, this->beta[0], this->d_p0, this->arrSize); // p0 = beta*p0
+        launchKernel(axpy<ITYPE, RTYPE>, grid, block, one, this->d_zk, this->d_p0, this->arrSize); // p0 = zk + beta*p0
+        POP_RANGE // 4: Update pk
+
+        POP_RANGE // 3: iteration
     }
     POP_RANGE // 2: iterations
 
@@ -397,7 +430,7 @@ void ConjugateGradient<ITYPE, RTYPE>::fpcgSolver(const MatVecOp &matvec, const P
     // 3. compute initial residual norm |r0| and initialize rk, rk.rk, p0
     TensorUtils<ITYPE, RTYPE>::copy_array(this->arrSize, this->r0, this->rk);              // rk = r0
     TensorUtils<ITYPE, RTYPE>::copy_array(this->arrSize, this->zk, this->p0);              // p0 = zk
-    TensorUtils<ITYPE, RTYPE>::ot_product(this->arrSize, this->rk, this->zk, this->resk);  // resk = rk . zk
+    TensorUtils<ITYPE, RTYPE>::dot_product(this->arrSize, this->rk, this->zk, this->resk);  // resk = rk . zk
     TensorUtils<ITYPE, RTYPE>::dot_product(this->arrSize, this->r0, this->r0, this->res0); // res0 = r0 . r0
     this->res0[0] = std::sqrt(this->res0[0]);                                              // res0 = |r0|
 
