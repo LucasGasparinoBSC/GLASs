6ede7bbc2e07b5d80af3f5009e46f55ff29b7484
Cian McDonnell <cipro@mcdonnell.eu>
2025-10-23 17:57:04 +0200

    Fix missing subclass

--- Apps/Diffusion1D/sources/mod_Diffusion1D.F90
+++ Apps/Diffusion1D/sources/mod_Diffusion1D.F90
@@ -1,5 +1,6 @@
 module mod_Diffusion1D
     use mod_Diffusion1D_Jacobian
+	use mod_Diffusion1D_Implicit
     use mod_Diffusion1D_Base
  
 	public
@@ -8,11 +9,11 @@ module mod_Diffusion1D
 
 	subroutine createDiffusionSolver(type, driver)
 
-		character(len=*),                   		 intent(in)    :: type
+		character(len=*),                   intent(in)    :: type
 		class(Diffusion1D_Base_t), pointer, intent(inout) :: driver
 
 		select case(trim(type))
-			case( "IMPLICIT" ); allocate(Diffusion1D_Base_t :: driver)
+			case( "IMPLICIT" ); allocate(Diffusion1D_Implicit_t :: driver)
 			case default;
 				write(*,*) "ERROR : createDiffusionSolver: Child class not implemented"
 				stop 1
