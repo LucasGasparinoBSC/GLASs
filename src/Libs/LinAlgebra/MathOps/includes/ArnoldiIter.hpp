8e5ef63585f277b51b05db736efe4af320b82c3c
Lucas Gasparino <lucas.gasparino@bsc.es>
2025-11-04 17:21:56 +0100

    To unify the communicator across all libs in LinAlgebra, EntryPoint was created as an abstract class from which certain common attributes are inherited. Also some initial work for the Arnoldi Iteration has been done, without testing. Finally, fixed the nvhpc workflow as i updated my computer.

--- src/Libs/LinAlgebra/MathOps/includes/ArnoldiIter.hpp
+++ src/Libs/LinAlgebra/MathOps/includes/ArnoldiIter.hpp
@@ -19,28 +19,28 @@
 #include <functional>
 #include <algorithm>
 
-#include "Comm_Utils.hpp"
-#include "TensorUtils.hpp"
+#include "EntryPoint.hpp"
 
 template <typename ITYPE, typename RTYPE>
-class ArnoldiIter
+class ArnoldiIter : public EntryPoint<ITYPE, RTYPE>
 {
     private:
     protected:
-        using MatVecOp = std::function<void(const RTYPE *x_in, RTYPE *x_out)>;
-        using ModVecOp = std::function<void(RTYPE *x_inout)>;
         ITYPE nRows;
         ITYPE maxIters;
         const ITYPE auxSize = 1;
-        RTYPE *wOld;
-        RTYPE *wNew;
-        RTYPE* UpHess;
-        RTYPE* Qkrylov;
-        RTYPE* aux;
-        double *tmpDot;
-        double *mpiTmp;
+        RTYPE *wOld, *d_wOld;
+        RTYPE *wNew, *d_wNew;
+        RTYPE *UpHess, *d_UpHess;
+        RTYPE *Qkrylov, *d_Qkrylov;
+        RTYPE *aux, *d_aux;
+        double *tmpDot, *d_tmpDot;
+        double *mpiTmp, *d_mpiTmp;
         bool flag_planned = false;
         bool flag_setup = false;
+
+        // Alias to inherited communication utilities
+        Comm_Utils& ArnoldiIter_comm = this->entrypoint_comm;
 
         void GramSchmidt(ITYPE kIter);
     public:
