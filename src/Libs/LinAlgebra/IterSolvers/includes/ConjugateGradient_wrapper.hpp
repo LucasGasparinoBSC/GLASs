28bf5451742f530fc065527a22c13ec6e797b843
Lucas Gasparino <lucas.gasparino@bsc.es>
2025-12-16 13:48:31 +0100

    Added wrappers for u32 double precision.

--- src/Libs/LinAlgebra/IterSolvers/includes/ConjugateGradient_wrapper.hpp
+++ src/Libs/LinAlgebra/IterSolvers/includes/ConjugateGradient_wrapper.hpp
@@ -11,14 +11,27 @@ extern "C" {
 
     // Matvec definitions
     typedef void (*matvec_f32)(const float *x_in, float *x_out, void *user_data);
+    typedef void (*matvec_f64)(const double *x_in, double *x_out, void *user_data);
+    typedef void (*precond_f32)(const float *r_in, float *z_out, void *user_data);
+    typedef void (*precond_f64)(const double *r_in, double *z_out, void *user_data);
 
     // ---- uint32_t / float ----
     void *cg_create_u32_f(uint32_t arrSize, uint32_t maxIters, double tol);                 // Serial version
     void *cg_create_u32_pf(MPI_Comm comm, uint32_t arrSize, uint32_t maxIters, double tol); // Parallel version
     void cg_destroy_u32_f(void *solver);
     void cg_setup_u32_f(void *solver, const float* inicond, const float* rhs);
-    void cg_solve_u32_f(void *solver, matvec_f32 matvec, void* user_data);
+    void cg_solve_u32_f(void *solver, matvec_f32 matvec, void *user_data);
+    void fpcg_solve_u32_f(void *solver, matvec_f32 matvec, precond_f32 precond, void *user_data);
     void cg_get_solution_u32_f(void *solver, float* sol);
+
+    // ---- uint32_t / double ----
+    void *cg_create_u32_d(uint32_t arrSize, uint32_t maxIters, double tol);                 // Serial version
+    void *cg_create_u32_pd(MPI_Comm comm, uint32_t arrSize, uint32_t maxIters, double tol); // Parallel version
+    void cg_destroy_u32_d(void *solver);
+    void cg_setup_u32_d(void *solver, const double* inicond, const double* rhs);
+    void cg_solve_u32_d(void *solver, matvec_f64 matvec, void* user_data);
+    void fpcg_solve_u32_d(void *solver, matvec_f64 matvec, precond_f64 precond, void* user_data);
+    void cg_get_solution_u32_d(void *solver, double* sol);
 
 #ifdef __cplusplus
 }
--- src/Libs/LinAlgebra/IterSolvers/sources/ConjugateGradient_F_wrapper.F90
+++ src/Libs/LinAlgebra/IterSolvers/sources/ConjugateGradient_F_wrapper.F90
@@ -2,6 +2,8 @@ module cg_wrapper_mod
     use iso_c_binding
     implicit none
     interface
+        !! ---- uint32_t / float ----
+
         ! Constructor call
         function cg_create_u32_f(arrSize, maxIters, tol) bind(C, name="cg_create_u32_f")
             import :: c_ptr, c_int32_t, c_double
@@ -44,7 +46,7 @@ module cg_wrapper_mod
 #endif
         end subroutine cg_setup_u32_f
 
-        ! Solver call
+        ! CG solver call
         subroutine cg_solve_u32_f(solver, matvec, user_data) bind(C, name="cg_solve_u32_f")
             import :: c_ptr, c_funptr
             implicit none
@@ -52,6 +54,16 @@ module cg_wrapper_mod
             type(c_funptr), value :: matvec
             type(c_ptr), value :: user_data
         end subroutine cg_solve_u32_f
+
+        ! FPCG solver call
+        subroutine fpcg_solve_u32_f(solver, matvec, precond, user_data) bind(C, name="fpcg_solve_u32_f")
+            import :: c_ptr, c_funptr
+            implicit none
+            type(c_ptr), value :: solver
+            type(c_funptr), value :: matvec
+            type(c_funptr), value :: precond
+            type(c_ptr), value :: user_data
+        end subroutine fpcg_solve_u32_f
 
         ! Get solution
         subroutine cg_get_solution_u32_f(solver, sol) bind(C, name="cg_get_solution_u32_f")
@@ -64,7 +76,81 @@ module cg_wrapper_mod
             real(c_float), intent(out) :: sol(*)
 #endif
         end subroutine cg_get_solution_u32_f
-        
+
+        !! ---- uint32_t / double ----
+
+        ! Constructor call
+        function cg_create_u32_d(arrSize, maxIters, tol) bind(C, name="cg_create_u32_d")
+            import :: c_ptr, c_int32_t, c_double
+            implicit none
+            integer(c_int32_t), value :: arrSize
+            integer(c_int32_t), value :: maxIters
+            real(c_double), value :: tol
+            type(c_ptr) :: cg_create_u32_d
+        end function cg_create_u32_d
+
+        ! Parallel constructor call
+        function cg_create_u32_pd(comm, arrSize, maxIters, tol) bind(C, name="cg_create_u32_pd")
+            import :: c_ptr, c_int32_t, c_double
+            implicit none
+            integer(c_int32_t), value :: comm
+            integer(c_int32_t), value :: arrSize
+            integer(c_int32_t), value :: maxIters
+            real(c_double), value :: tol
+            type(c_ptr) :: cg_create_u32_pd
+        end function cg_create_u32_pd
+
+        ! Destructor call
+        subroutine cg_destroy_u32_d(solver) bind(C, name="cg_destroy_u32_d")
+            import :: c_ptr
+            implicit none
+            type(c_ptr), value :: solver
+        end subroutine cg_destroy_u32_d
+
+        ! Setup
+        subroutine cg_setup_u32_d(solver, inicond, rhs) bind(C, name="cg_setup_u32_d")
+            import :: c_ptr, c_double
+            implicit none
+            type(c_ptr), value :: solver
+#ifdef USE_GPU
+            real(c_double), device, intent(in) :: inicond(*)
+            real(c_double), device, intent(in) :: rhs(*)
+#else
+            real(c_double), intent(in) :: inicond(*)
+            real(c_double), intent(in) :: rhs(*)
+#endif
+        end subroutine cg_setup_u32_d
+
+        ! CG solver call
+        subroutine cg_solve_u32_d(solver, matvec, user_data) bind(C, name="cg_solve_u32_d")
+            import :: c_ptr, c_funptr
+            implicit none
+            type(c_ptr), value :: solver
+            type(c_funptr), value :: matvec
+            type(c_ptr), value :: user_data
+        end subroutine cg_solve_u32_d
+
+        ! FPCG solver call
+        subroutine fpcg_solve_u32_d(solver, matvec, precond, user_data) bind(C, name="fpcg_solve_u32_d")
+            import :: c_ptr, c_funptr
+            implicit none
+            type(c_ptr), value :: solver
+            type(c_funptr), value :: matvec
+            type(c_funptr), value :: precond
+            type(c_ptr), value :: user_data
+        end subroutine fpcg_solve_u32_d
+
+        ! Get solution
+        subroutine cg_get_solution_u32_d(solver, sol) bind(C, name="cg_get_solution_u32_d")
+            import :: c_ptr, c_double
+            implicit none
+            type(c_ptr), value :: solver
+#ifdef USE_GPU
+            real(c_double), device, intent(out) :: sol(*)
+#else
+            real(c_double), intent(out) :: sol(*)
+#endif
+        end subroutine cg_get_solution_u32_d
     end interface
     ! Abstract interface for matvec
     abstract interface
@@ -75,5 +161,29 @@ module cg_wrapper_mod
             real(kind=4), intent(out) :: x_out(*)
             type(c_ptr), value :: user_data
         end subroutine matvec_f32
+
+        subroutine matvec_f64(x_in, x_out, user_data) bind(C)
+            use iso_c_binding, only : c_ptr
+            implicit none
+            real(kind=8), intent(in) :: x_in(*)
+            real(kind=8), intent(out) :: x_out(*)
+            type(c_ptr), value :: user_data
+        end subroutine matvec_f64
+
+        subroutine precond_f32(r_in, z_out, user_data) bind(C)
+            use iso_c_binding, only : c_ptr
+            implicit none
+            real(kind=4), intent(in) :: r_in(*)
+            real(kind=4), intent(out) :: z_out(*)
+            type(c_ptr), value :: user_data
+        end subroutine precond_f32
+
+        subroutine precond_f64(r_in, z_out, user_data) bind(C)
+            use iso_c_binding, only : c_ptr
+            implicit none
+            real(kind=8), intent(in) :: r_in(*)
+            real(kind=8), intent(out) :: z_out(*)
+            type(c_ptr), value :: user_data
+        end subroutine precond_f64
     end interface
 end module cg_wrapper_mod
