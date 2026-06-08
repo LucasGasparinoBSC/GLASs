e384f764854b67732c40df8372e30e425e64f9ed
Lucas Gasparino <lucas.gasparino@bsc.es>
2026-06-08 15:40:24 +0200

    Well.... this is the ONLY way i can get this shit working on LUMI and MN5.... because fuck AMD :D

--- src/Libs/Kernels_Lv1/includes/Kernels_Lv1.cuh
+++ src/Libs/Kernels_Lv1/includes/Kernels_Lv1.cuh
@@ -40,103 +40,126 @@
     #define MAX_BLOCKS 10240
 #endif
 
-template <typename ITYPE, typename RTYPE>
-__global__ void axpy(const RTYPE a, const RTYPE* x, RTYPE* y, const ITYPE N) {
-    ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
-    while (gid < N) {
-        y[gid] += a * x[gid];
-        gid += blockDim.x * gridDim.x;
-    }
-}
-
-template <typename ITYPE, typename RTYPE>
-__global__ void dot_product(const RTYPE* a, const RTYPE* b, double* r, ITYPE N) {
-    // Indexes
-    ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
-    ITYPE tid = threadIdx.x;
-
-    // Partials
-    __shared__ float cache[TILE_SIZE];
-    double value = 0.0;
-    while (gid < N) {
-        value += static_cast<double>(a[gid] * b[gid]);
-        gid += blockDim.x * gridDim.x;
-    }
-    cache[tid] = value;
-    __syncthreads();
-
-    // Reduction in shared memory
-    ITYPE i = blockDim.x / 2;
-    while (i != 0) {
-        if (tid < i) {
-            cache[tid] += cache[tid + i];
+// For CUDA, full implementation is here. For AMD, the implementation is in the .cu file to avoid HIP compilation issues with templates.
+#if defined(USE_CUDA)
+    template <typename ITYPE, typename RTYPE>
+    __global__ void axpy(const RTYPE a, const RTYPE* x, RTYPE* y, const ITYPE N) {
+        ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
+        while (gid < N) {
+            y[gid] += a * x[gid];
+            gid += blockDim.x * gridDim.x;
         }
+    }
+
+    template <typename ITYPE, typename RTYPE>
+    __global__ void dot_product(const RTYPE* a, const RTYPE* b, double* r, ITYPE N) {
+        // Indexes
+        ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
+        ITYPE tid = threadIdx.x;
+
+        // Partials
+        __shared__ float cache[TILE_SIZE];
+        double value = 0.0;
+        while (gid < N) {
+            value += static_cast<double>(a[gid] * b[gid]);
+            gid += blockDim.x * gridDim.x;
+        }
+        cache[tid] = value;
         __syncthreads();
-        i /= 2;
+
+        // Reduction in shared memory
+        ITYPE i = blockDim.x / 2;
+        while (i != 0) {
+            if (tid < i) {
+                cache[tid] += cache[tid + i];
+            }
+            __syncthreads();
+            i /= 2;
+        }
+
+        // Atomic add to global result
+        if (tid == 0) {
+            atomicAdd(r, cache[0]);
+        }
     }
 
-    // Atomic add to global result
-    if (tid == 0) {
-        atomicAdd(r, cache[0]);
+    template <typename ITYPE, typename RTYPE>
+    __global__ void scale(const RTYPE a, RTYPE* x, ITYPE N) {
+        ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
+        while (gid < N) {
+            x[gid] *= a;
+            gid += blockDim.x * gridDim.x;
+        }
     }
-}
 
-template <typename ITYPE, typename RTYPE>
-__global__ void scale(const RTYPE a, RTYPE* x, ITYPE N) {
-    ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
-    while (gid < N) {
-        x[gid] *= a;
-        gid += blockDim.x * gridDim.x;
+    template <typename ITYPE, typename RTYPE>
+    __global__ void copy_array(const RTYPE* x, RTYPE* y, ITYPE N) {
+        ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
+        while (gid < N) {
+            y[gid] = x[gid];
+            gid += blockDim.x * gridDim.x;
+        }
     }
-}
 
-template <typename ITYPE, typename RTYPE>
-__global__ void copy_array(const RTYPE* x, RTYPE* y, ITYPE N) {
-    ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
-    while (gid < N) {
-        y[gid] = x[gid];
-        gid += blockDim.x * gridDim.x;
+    template <typename ITYPE, typename RTYPE>
+    __global__ void array_sqrt(RTYPE* x, ITYPE N) {
+        ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
+        double value;
+        while (gid < N) {
+            value = static_cast<double>(x[gid]);
+            x[gid] = static_cast<RTYPE>(std::sqrt(value));
+            gid += blockDim.x * gridDim.x;
+        }
     }
-}
 
-template <typename ITYPE, typename RTYPE>
-__global__ void array_sqrt(RTYPE* x, ITYPE N) {
-    ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
-    double value;
-    while (gid < N) {
-        value = static_cast<double>(x[gid]);
-        x[gid] = static_cast<RTYPE>(std::sqrt(value));
-        gid += blockDim.x * gridDim.x;
+    template <typename ITYPE, typename RTYPE>
+    __global__ void array_invert(RTYPE* x, ITYPE N) {
+        ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
+        double value;
+        while (gid < N) {
+            value = static_cast<double>(x[gid]);
+            x[gid] = static_cast<RTYPE>(1.0 / value);
+            gid += blockDim.x * gridDim.x;
+        }
     }
-}
 
-template <typename ITYPE, typename RTYPE>
-__global__ void array_invert(RTYPE* x, ITYPE N) {
-    ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
-    double value;
-    while (gid < N) {
-        value = static_cast<double>(x[gid]);
-        x[gid] = static_cast<RTYPE>(1.0 / value);
-        gid += blockDim.x * gridDim.x;
+    template <typename ITYPE, typename RTYPE>
+    __global__ void pointwise_multiply(const RTYPE* x, RTYPE* y, ITYPE N) {
+        ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
+        while (gid < N) {
+            y[gid] *= x[gid];
+            gid += blockDim.x * gridDim.x;
+        }
     }
-}
 
-template <typename ITYPE, typename RTYPE>
-__global__ void pointwise_multiply(const RTYPE* x, RTYPE* y, ITYPE N) {
-    ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
-    while (gid < N) {
-        y[gid] *= x[gid];
-        gid += blockDim.x * gridDim.x;
+    template <typename ITYPE, typename RTYPE>
+    __global__ void set_array(RTYPE* x, RTYPE value, ITYPE N) {
+        ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
+        while (gid < N) {
+            x[gid] = value;
+            gid += blockDim.x * gridDim.x;
+        }
     }
-}
 
-template <typename ITYPE, typename RTYPE>
-__global__ void set_array(RTYPE* x, RTYPE value, ITYPE N) {
-    ITYPE gid = blockIdx.x * blockDim.x + threadIdx.x;
-    while (gid < N) {
-        x[gid] = value;
-        gid += blockDim.x * gridDim.x;
-    }
-}
+// Just declare the kernels for AMD, implementation is in the .cu file to avoid HIP compilation issues with templates.
+#elif defined(USE_HIP)
+    template <typename ITYPE, typename RTYPE>
+    __global__ void axpy(const RTYPE a, const RTYPE* x, RTYPE* y, const ITYPE N);
+    template <typename ITYPE, typename RTYPE>
+    __global__ void dot_product(const RTYPE* a, const RTYPE* b, double* r, ITYPE N);
+    template <typename ITYPE, typename RTYPE>
+    __global__ void scale(const RTYPE a, RTYPE* x, ITYPE N);
+    template <typename ITYPE, typename RTYPE>
+    __global__ void copy_array(const RTYPE* x, RTYPE* y, ITYPE N);
+    template <typename ITYPE, typename RTYPE>
+    __global__ void array_sqrt(RTYPE* x, ITYPE N);
+    template <typename ITYPE, typename RTYPE>
+    __global__ void array_invert(RTYPE* x, ITYPE N);
+    template <typename ITYPE, typename RTYPE>
+    __global__ void pointwise_multiply(const RTYPE* x, RTYPE* y, ITYPE N);
+    template <typename ITYPE, typename RTYPE>
+    __global__ void set_array(RTYPE* x, RTYPE value, ITYPE N);
+#endif
+
 
 #endif //! __KERNELS_LV1_CUH__
