319ed053ad8dc5a9fb96b84a733ff5b170bb02b4
Lucas Gasparino <lucas.gasparino@bsc.es>
2026-06-09 13:42:30 +0200

    Applied same "fix" to Conjgrad test.

--- Tests/ConjugateGradient/DeviceSide.cuh
+++ Tests/ConjugateGradient/DeviceSide.cuh
@@ -3,13 +3,18 @@
 
 #include "DeviceUtils.hpp"
 
-template <typename ITYPE, typename RTYPE>
-__global__ void diagMatVec_device(const RTYPE* Adiag, const RTYPE* x_in, RTYPE* x_out, ITYPE N) {
-    ITYPE idx = blockIdx.x * blockDim.x + threadIdx.x;
-    while (idx < N) {
-        x_out[idx] = Adiag[idx] * x_in[idx];
-        idx += blockDim.x * gridDim.x;
+#if defined (USE_CUDA)
+    template <typename ITYPE, typename RTYPE>
+    __global__ void diagMatVec_device(const RTYPE* Adiag, const RTYPE* x_in, RTYPE* x_out, ITYPE N) {
+        ITYPE idx = blockIdx.x * blockDim.x + threadIdx.x;
+        while (idx < N) {
+            x_out[idx] = Adiag[idx] * x_in[idx];
+            idx += blockDim.x * gridDim.x;
+        }
     }
-}
+#elif defined (USE_HIP)
+    template <typename ITYPE, typename RTYPE>
+    __global__ void diagMatVec_device(const RTYPE* Adiag, const RTYPE* x_in, RTYPE* x_out, ITYPE N);
+#endif
 
 #endif //! DEVICE_SIDE_CUH
